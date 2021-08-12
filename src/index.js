d1976bc8c4fa24513cc33d7d67c5fb60f35559a9
roman kostyrin <enemy-iubip@mail.ru>
2021-08-13 00:31:07 +0300

    init: create html file with sudoku

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-module.exports = function solveSudoku(matrix) {
-  // your solution
-}
+// module.exports = function solveSudoku(matrix) {
+//   // your solution
+// }
+let cell = document.querySelectorAll('.cell')
+
+const initial = [
+  [6, 5, 0, 7, 3, 0, 0, 8, 0],
+  [0, 0, 0, 4, 8, 0, 5, 3, 0],
+  [8, 4, 0, 9, 2, 5, 0, 0, 0],
+  [0, 9, 0, 8, 0, 0, 0, 0, 0],
+  [5, 3, 0, 2, 0, 9, 6, 0, 0],
+  [0, 0, 6, 0, 0, 0, 8, 0, 0],
+  [0, 0, 9, 0, 0, 0, 0, 0, 6],
+  [0, 0, 7, 0, 0, 0, 0, 5, 0],
+  [1, 6, 5, 3, 9, 0, 4, 7, 0],
+]
+
+function solveSudoku(matrix) {}
+let counter = 0
+initial.forEach((el, index) => {
+  el.forEach((value, ind) => {
+    cell[counter].innerHTML = value
+    counter++
+  })
+})
